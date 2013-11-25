3bffcc41e56eeba00c91d03243749c22bbd227b7
Eric Buth <ericdotbuth@gmail.com>
2013-11-25 11:49:30 -0500

    working initialization

--- stocker.go
+++ stocker.go
@@ -5,17 +5,18 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/buth/stocker/stocker/backend"
+	"github.com/buth/stocker/stocker/backend/redisc"
 	"github.com/buth/stocker/stocker/crypto"
 	"github.com/buth/stocker/stocker/crypto/chain"
-	"github.com/coreos/go-etcd/etcd"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 var config struct {
-	GenerateKey, RunDaemon                       bool
-	SecretFilepath, Crypter, EtcdURL, Key, Value string
+	GenerateKey, RunDaemon                                                           bool
+	SecretFilepath, Crypter, Backend, BackendConnectionType, BackendConnectionString string
 }
 
 // genereateKey creates and returns a new key for the chosen crypter as a
@@ -28,23 +29,42 @@ func generateKey() []byte {
 	return []byte{}
 }
 
-// crypterFromFile instantiates a new crypter of the chosen type using the
-// base 64 encoded key present in the file at filepath.
-func crypterFromFile(filepath string) (crypto.Crypter, error) {
+// crypter instantiates a new crypter of the chosen type using a new key or
+// the base 64 encoded key present in the file at config.Secret.
+func newCrypter() (crypto.Crypter, error) {
 
-	// Attempt to read the entire content of the file at filepath.
-	encodedContent, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return nil, err
+	var key []byte
+
+	if config.SecretFilepath != "" {
+
+		// Check the status of the secret file.
+		stat, err := os.Stat(config.SecretFilepath)
+		if err != nil {
+			return nil, err
+		}
+
+		// Only proceed if the running user is the only user that can read the
+		// secret.
+		if stat.Mode() != 0600 && stat.Mode() != 0400 {
+			return nil, errors.New("incorrect secret file permissions!")
+		}
+
+		// Attempt to read the entire content of the secret file.
+		encodedKey, err := ioutil.ReadFile(config.SecretFilepath)
+		if err != nil {
+			return nil, err
+		}
+
+		// Attempt to decode the encoded content into a new slice of bytes.
+		key = make([]byte, len(encodedKey)*4)
+		base64.StdEncoding.Decode(key, encodedKey)
+	} else {
+		key = generateKey()
 	}
-
-	// Attempt to decode the encoded content into a new slice of bytes.
-	content := make([]byte, len(encodedContent)*4)
-	base64.StdEncoding.Decode(content, encodedContent)
 
 	switch config.Crypter {
 	case "chain":
-		newChain, err := chain.New(content)
+		newChain, err := chain.New(key)
 		if err != nil {
 			return nil, err
 		}
@@ -54,13 +74,25 @@ func crypterFromFile(filepath string) (crypto.Crypter, error) {
 	return nil, errors.New("no crypter selected")
 }
 
+func newBackend() (backend.Backend, error) {
+
+	switch config.Backend {
+	case "redis":
+		newRedisc := redisc.New(config.BackendConnectionType, config.BackendConnectionString)
+		return newRedisc, nil
+	}
+
+	return nil, errors.New("no backend selected")
+}
+
 func init() {
 	flag.BoolVar(&config.GenerateKey, "k", false, "generate key")
 	flag.BoolVar(&config.RunDaemon, "d", false, "run daemon")
-	flag.StringVar(&config.SecretFilepath, "s", "key.txt", "path to encryption secret")
-	flag.StringVar(&config.Crypter, "m", "chain", "crypter to use")
-	flag.StringVar(&config.Key, "key", "", "key")
-	flag.StringVar(&config.Value, "value", "", "value")
+	flag.StringVar(&config.Crypter, "crypter", "chain", "crypter to use")
+	flag.StringVar(&config.SecretFilepath, "secret", "", "path to encryption secret")
+	flag.StringVar(&config.Backend, "backend", "redis", "backend to use")
+	flag.StringVar(&config.BackendConnectionType, "t", "tcp", "backend connection type")
+	flag.StringVar(&config.BackendConnectionString, "s", "127.0.0.1:6379", "backend connection string")
 	flag.Parse()
 }
 
@@ -72,65 +104,17 @@ func main() {
 		return
 	}
 
-	// Check the status of the secret file.
-	stat, err := os.Stat(config.SecretFilepath)
+	// Attempt to load a crypter.
+	crypter, err := newCrypter()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Only proceed if the running user is the only user that can read the
-	// secret.
-	if stat.Mode() != 0600 && stat.Mode() != 0400 {
-		log.Fatalln("incorrect secret file permissions!")
-	}
-
-	// Attempt to load a crypter from the key provided in the secret filepath
-	crypter, err := crypterFromFile(config.SecretFilepath)
+	// Attempt to load a backend.
+	backend, err := newBackend()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Right now, we're limited to expecting etcd to be running on localhost, so
-	// we'll just use the NewClient method provided by the etcd client.
-	client := etcd.NewClient()
-
-	// Check a key was provided. Arguably, providing a value in the absense of a
-	// key could be an error, but for the moment that's not implemented.
-	if config.Key != "" {
-
-		// Check if a value was provided. In that case, this is a set operation.
-		if config.Value != "" {
-
-			// Encrypt the provided value before sending it.
-			encryptedValue, err := crypter.EncryptString(config.Value)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			// Run the set command with the encrypted value.
-			response, err := client.Set(config.Key, encryptedValue, 0)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			log.Println(response)
-		} else {
-
-			// Run a get command to retrieve the encrypted value.
-			responses, err := client.Get(config.Key)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			// Range over the responses, throwing away the index.
-			for _, response := range responses {
-
-				// Decrypt the individual response.
-				decryptedValue, err := crypter.DecryptString(response.Value)
-				if err != nil {
-					log.Fatalln(err)
-				}
-				fmt.Println(decryptedValue)
-			}
-		}
-	}
+	log.Println(crypter, backend)
 }
--- stocker/backend/backend.go
+++ stocker/backend/backend.go
@@ -4,5 +4,5 @@ type Backend interface {
 	Get(key string) (value string, err error)
 	Set(key, value string) (err error)
 	SetWithTTL(key, value string, ttl int) (err error)
-	Remove(keystring) (err error)
+	Remove(key string) (err error)
 }
