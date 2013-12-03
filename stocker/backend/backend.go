aed8a8312d216a7d41d6748e31670ad59ce1464f
Eric Buth <ericdotbuth@gmail.com>
2013-12-03 10:53:47 -0500

    universal key method

--- stocker/backend/backend.go
+++ stocker/backend/backend.go
@@ -1,11 +1,19 @@
 package backend
 
+import (
+	"strings"
+)
+
 type Backend interface {
 	Get(key string) (string, error)
 	Set(key, value string) error
 	SetWithTTL(key, value string, ttl int) error
 	Remove(key string) error
 	Publish(key, message string) error
-	Subscribe(key string, process func(channel, message string) error) error
+	Subscribe(pattern string, process func(key, message string) error) error
 	Unsubscribe(key string) error
+}
+
+func Key(components ...string) string {
+	return strings.Join(components, "/")
 }
