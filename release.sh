5018acd826664b79be63996269af7bd8b374476e
Eric Buth <ericdotbuth@gmail.com>
2014-04-17 15:59:25 -0400

    release

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+VERSION=0.0.1
+RELEASE_BRANCH=master
+
+if [[ $DRONE_BRANCH != $RELEASE_BRANCH ]]; then
+	echo "Only publish a release for the $RELEASE_BRANCH branch."
+	exit 0
+fi
+
+gox -output="build/stocker-v$VERSION-{{.OS}}-{{.Arch}}/bin/stocker" -os="linux darwin"
+
+cd build
+
+for dir in `ls`
+do
+	cp ../README.md ../LICENSE $dir/
+	tar -zcvf $dir.tar.gz $dir
+	rm -r $dir
+done
+
+shasum *.tar.gz > SHASUMS.txt
+
+for file in `ls`
+do
+	aws s3 cp $file s3://newsdev-pub/stocker/$VERSION/$file
+done
