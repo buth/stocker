08ac7699ac4a533dee6b562f253e371a6cf7a922
Eric Buth <ericdotbuth@gmail.com>
2014-05-22 13:52:26 -0400

    getgroup backend functionality and -E flag for exec, fixes #10

--- backend/backend_test.go
+++ backend/backend_test.go
@@ -10,6 +10,12 @@ var testBackends = []struct {
 	{"redis", "redistest", "tcp", ":6379"},
 }
 
+var testBackendsPairs = map[string]string{
+	"TESTVARIABLE1": "TESTVALUE1",
+	"TESTVARIABLE2": "TESTVALUE3",
+	"TESTVARIABLE3": "TESTVALUE3",
+}
+
 func TestBackend(t *testing.T) {
 	for _, b := range testBackends {
 
@@ -18,17 +24,53 @@ func TestBackend(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		if err := backend.SetVariable("testgroup", "TESTVARIABLE", "testvalue"); err != nil {
+		for variable, value := range testBackendsPairs {
+			if err := backend.SetVariable("testgroup", variable, value); err != nil {
+				t.Error(err)
+			}
+		}
+
+		for variable, value := range testBackendsPairs {
+			v, err := backend.GetVariable("testgroup", variable)
+			if err != nil {
+				t.Error(err)
+			} else if v != value {
+				t.Errorf("expected value %s for %s but found %s!", value, variable, v)
+			}
+		}
+
+		if err := backend.RemoveGroup("testgroup"); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestBackendGetAll(t *testing.T) {
+	for _, b := range testBackends {
+
+		backend, err := NewBackend(b.Kind, b.Namespace, b.Protocol, b.Address)
+		if err != nil {
 			t.Fatal(err)
 		}
 
-		value, err := backend.GetVariable("testgroup", "TESTVARIABLE")
+		for variable, value := range testBackendsPairs {
+			if err := backend.SetVariable("testgroup", variable, value); err != nil {
+				t.Error(err)
+			}
+		}
+
+		variables, err := backend.GetGroup("testgroup")
 		if err != nil {
 			t.Error(err)
 		}
 
-		if value != "testvalue" {
-			t.Error("value did not match!")
+		for variable, value := range testBackendsPairs {
+			v, ok := variables[variable]
+			if !ok {
+				t.Errorf("no value returned for %s!", variable)
+			} else if v != value {
+				t.Errorf("expected value %s for %s but found %s!", value, variable, v)
+			}
 		}
 
 		if err := backend.RemoveGroup("testgroup"); err != nil {
