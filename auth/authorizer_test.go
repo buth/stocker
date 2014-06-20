52d66989cad07cb7915cd56dc444e4897283ba92
Eric Buth <ericdotbuth@gmail.com>
2014-06-20 16:09:24 -0400

    write permissions, better testing

--- auth/authorizer_test.go
+++ auth/authorizer_test.go
@@ -11,33 +11,109 @@ var TestAuthorizerKeys = [][]byte{
 	[]byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjEDzUrQo+eiAqgxRBJzrY9EPpTq4CyVDm9NMs6MinZ06PQLbxshZxruxrsrZtfnps1cz0HYqFCkSLTk/2KYdpstYxb0af2k6xxmGtTNdCDQTD3MThW+T3Qv58wXo75EgFI3ZEgGgo/bcALpaMYN0skil4cSv/Vd1B36tKgF6QKf3vFSsiiVJ8pPrM1v32DJ/DvLvQb5VsPrDfTcMqsfNXyRjXNMZqGmK2uAlZBcbO6o4M+yrRtt9wRnTJvAdLIip+ZF2O1q28atlluUWRrneW39oy9p/M5w/Gw/BuCRoshZadBovziXdHnlCh00JbM/xS+m1GsDXhfWIYuSXqTSCd key3"),
 }
 
-func TestAuthorizer(t *testing.T) {
-	authorizer := NewAuthorizer()
+func testAuthorizerKeysParsed() ([]ssh.PublicKey, error) {
 
 	publicKeys := make([]ssh.PublicKey, len(TestAuthorizerKeys))
 
 	for i, rawKey := range TestAuthorizerKeys {
 		publicKey, _, _, _, err := ssh.ParseAuthorizedKey(rawKey)
 		if err != nil {
-			t.Fatal(err)
+			return publicKeys, err
 		}
 
 		// Save the key.
 		publicKeys[i] = publicKey
 	}
 
-	for _, publicKey := range publicKeys {
-		if authorizer.Authorize(publicKey) {
-			t.Error("authorized a key that hasn't been added.")
-		}
+	return publicKeys, nil
+}
 
-		// Add the key to the authorizer.
-		authorizer.AddKey(publicKey)
+func TestAuthorizerKeyTotals(t *testing.T) {
+
+	if l := len(TestAuthorizerKeys); l != 3 {
+		t.Fatal("wrong number of test keys:", l)
+	}
+
+	publicKeys, err := testAuthorizerKeysParsed()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if l := len(publicKeys); l != 3 {
+		t.Fatal("wrong number of parsed test keys:", l)
+	}
+}
+
+func TestAuthorizer(t *testing.T) {
+	authorizer := NewAuthorizer()
+
+	publicKeys, err := testAuthorizerKeysParsed()
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	for _, publicKey := range publicKeys {
-		if !authorizer.Authorize(publicKey) {
-			t.Error("failed to authorized a key that has been added.")
+		if authorized, _ := authorizer.Authorize(publicKey); authorized {
+			t.Fatal("authorized a key that hasn't been added.")
+		}
+
+		// Add the key to the authorizer.
+		authorizer.AddKey(publicKey, false)
+	}
+
+	for _, publicKey := range publicKeys {
+		if authorized, _ := authorizer.Authorize(publicKey); !authorized {
+			t.Fatal("failed to authorized a key that has been added.")
+		}
+	}
+}
+
+func TestAuthorizerWriteNotPermitted(t *testing.T) {
+	authorizer := NewAuthorizer()
+
+	publicKeys, err := testAuthorizerKeysParsed()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, publicKey := range publicKeys {
+		authorizer.AddKey(publicKey, false)
+	}
+
+	for _, publicKey := range publicKeys {
+
+		authorized, canWrite := authorizer.Authorize(publicKey)
+		if !authorized {
+			t.Fatal("failed to authorized a key that has been added.")
+		}
+
+		if canWrite {
+			t.Error("allowing writes with non-writer key")
+		}
+	}
+}
+
+func TestAuthorizerWritePermitted(t *testing.T) {
+	authorizer := NewAuthorizer()
+
+	publicKeys, err := testAuthorizerKeysParsed()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, publicKey := range publicKeys {
+		authorizer.AddKey(publicKey, true)
+	}
+
+	for _, publicKey := range publicKeys {
+
+		authorized, canWrite := authorizer.Authorize(publicKey)
+		if !authorized {
+			t.Fatal("failed to authorized a key that has been added.")
+		}
+
+		if !canWrite {
+			t.Error("disallowing writes with writer key")
 		}
 	}
 }
